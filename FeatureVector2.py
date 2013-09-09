058d6358fb791c5cbfa837bee17d3781d46bff62
LunasAbacus <lunasabacus@gmail.com>
2013-09-09 11:35:56 -0400

    Added file for josh to add code to

---
+++ FeatureVector2.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ShiroRaven
+#
+# Created:     09/09/2013
+# Copyright:   (c) ShiroRaven 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
