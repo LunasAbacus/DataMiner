6d3a7b43f269f7e49f0d227320a5ac86fd0f1e2c
Joshua Adams <N7down@gmail.com>
2013-09-09 16:17:39 -0400

    updated FeaturedVector1

--- FeatureVector1.py
+++ FeatureVector1.py
@@ -12,6 +12,7 @@
 
 from TagExtractor import ReuterRooter as RR
 import nltk
+from decimal import Decimal
 
 def findtags(tag_prefix, tagged_text):
     cfd = nltk.ConditionalFreqDist((tag, word) for (word, tag) in tagged_text
@@ -30,6 +31,12 @@ def printFrequentNouns(body):
     print
 
 def main():
+    
+    blackList = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+',
+                 '-', '=', '{', '}', '|', '[', ']', '\\', ']', ';', '\'', ':', '"',
+                 ',', '.', '/', '<', '>', '?', 'to', 'a', 'is', 'and', 'be', 'the',
+                 'of']
+    
     sgm = RR('reut2-000.sgm')
 
     #print out all article names
@@ -41,21 +48,22 @@ def main():
     #print(sgm.ExtractTagData(0, "TITLE"))
     #print
 
-    for i in range(0, num):
-        print(sgm.ExtractTagData(i, "TITLE"))
-        printFrequentNouns(sgm.ExtractTagData(i, "BODY"))
+    #print frequuent nouns
+    #for i in range(0, num):
+        #print(sgm.ExtractTagData(i, "TITLE"))
+        #printFrequentNouns(sgm.ExtractTagData(i, "BODY"))
 
     #tokenize the body - convert to lower casing
-    #body = sgm.ExtractTagData(0, "BODY").lower()
-    #tokens = nltk.word_tokenize(body)
-    #print tokens
-    #print
+    body = sgm.ExtractTagData(0, "BODY").lower()
+    tokens = nltk.word_tokenize(body)
+    print tokens
+    print
     
     #convert tokens to lower casing
     #for i in range(0, len(tokens)):
     #tokens[i] = tokens[i].lower()
-    #print tokens
-    #print
+            #print tokens
+            #print
 
     #create the tags
     #tagged = nltk.pos_tag(tokens)
@@ -69,20 +77,61 @@ def main():
         #print tag, tagdict[tag]
         #print tagdict[tag]
     #print
+    
+    #the featured vector
+    #featuredVector = []
+    
+    #construct the featured vector
+    #for i in range(0, len(tagdict)):
+        #if(not tagdict[i] in blackList):
+            #featuredVector.append(tagdict[i])
+
+    #print featuredVector
+    #print
 
     #the dictionary
-    #d = {}
+    d = {}
+
+    #number of words
+    numberOfWords = 0
 
     #put the tokens into a dictionary - count the number of words that
-    # appear in the list
-    #numberOfTokens = len(tokens)
-    #for i in range(0, numberOfTokens):
-        #if not d.has_key(tokens[i]):
-            #d[tokens[i]] = 1
-        #elif d.has_key(tokens[i]):
-            #d[tokens[i]] = d[tokens[i]] + 1
+    #appear in the list
+    numberOfTokens = len(tokens)
+    for i in range(0, numberOfTokens):
+        if not d.has_key(tokens[i]):
+            d[tokens[i]] = 1
+            numberOfWords = numberOfWords + 1
+        elif d.has_key(tokens[i]):
+            d[tokens[i]] = d[tokens[i]] + 1
+            numberOfWords = numberOfWords + 1
 
-    #print(d)
+    print(d)
+    print
+
+    print numberOfWords
+    print
+
+    keys = d.keys()
+    print keys
+    print
+
+    lengthOfDict = len(d)
+    print lengthOfDict
+    print
+
+    tupleList = []
+    for i in range(0, lengthOfDict):
+        #if not in the blackList then add it, dont add it otherwise
+        if(not keys[i] in blackList):
+            tuple = (format((d[keys[i]] / float(numberOfWords)), '0.10f'), d[keys[i]], keys[i])
+            tupleList.append(tuple)
+            print tuple
+
+    print
+
+    print sorted(tupleList)
+    print
 
 if __name__ == '__main__':
     main()
